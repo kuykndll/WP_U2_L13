cae5d3cf6db057c9a073c27e364f5cf8a9a912e8
Jonas <airfryerkitten@gmail.com>
2025-11-20 09:56:07 -0500

    adding index JS

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ class Card{
         this.position = position
     }
 }
+
+function genCards(){
+    if (sessionStorage.getItem("game_status") == null){
+        console.log("true")
+        sessionStorage.setItem("game_status","true")
+    }
+    else{
+        console.log("game is being played")
+    }
+
+}
